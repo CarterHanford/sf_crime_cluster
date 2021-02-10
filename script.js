fc573ca55b7d18c7547fcaeaddf8ced96a56aa91
Carter Hanford <carter.hanford@slu.edu>
2021-02-10 11:53:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+// generate map and set location
+var map = L.map('map').setView([37.745, -122.4], 11.5);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [-90, 38] // longitude, latitude
-        });
-      });
+// set map style
+var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+// add crime and cluster
+ $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){
+    var ratIcon = L.icon({
+      iconUrl: 'https://c0.klipartz.com/pngpicture/392/290/gratis-png-robo-robo-ladrones.png',
+      iconSize: [60,50]
+    });
+    var rodents = L.geoJson(data,{
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: ratIcon});
+        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT);
+        return marker;
+      }
+    });
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(rodents);
+    map.addLayer(clusters);
+});
